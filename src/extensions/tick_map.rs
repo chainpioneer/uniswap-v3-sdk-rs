935561217482f581fba4d37dc41be65a6dba3c5c
chainpioneer <chainpioneer@gmail.com>
2025-05-23 17:44:30 -0300

    fix

--- src/extensions/tick_map.rs
+++ src/extensions/tick_map.rs
@@ -7,7 +7,7 @@ use alloc::vec::Vec;
 use alloy_primitives::{aliases::I24, map::rustc_hash::FxHashMap, uint, U256};
 use serde::Serialize;
 
-#[derive(Clone, Debug, Serialize, PartialEq, Eq, Default)]
+#[derive(Clone, Debug, Serialize, Default)]
 pub struct TickMap<I = I24> {
     pub bitmap: TickBitMap<I>,
     pub inner: FxHashMap<I, Tick<I>>,
